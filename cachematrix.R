0c99f7d86ac946ec9471cb57adc6fdfc3fb691ef
ConstantinaG <k.galbogini@gmail.com>
2023-12-20 18:18:30 +0200

    updated

--- cachematrix.R
+++ cachematrix.R
@@ -3,30 +3,50 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
+## build the setters & the getters of the matrix both the matrix & the inversed matrix
+makeCacheMatrix <- function(mtrx = matrix()) {
+  inverse <- NULL
+  
+  ## setter of the matrix
+  set <- function(m){
+    mtrx <<- m
+    inverse <<- NULL
   }
-  get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
-  getInverse <- function() inv
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  
+  ## getter of the matrix
+  get <- function() {
+    mtrx
+  }
+  
+  ## setter of the inverse of matrix
+  setInversedData <- function(solvedMtrx) {
+    inverse <<- solvedMtrx
+  }
+  
+  ## getter of the inverse of matrix
+  getInversedData <- function() {
+    inverse
+  }
+  
+  list("a" = 1, "b" = 2, "c" = 3, "d" = 4, "e" = 5, "f" = 6)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if(!is.null(inv)){
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv      
+cacheSolve <- function(mtrx, ...) {
+  returnedData <- mtrx$getInversedData()
+  
+  ## if matrix is not empty/null we have the inverse data
+  if(!is.null(returnedData)) return(returnedData)
+  
+  ## assign the data of the matrix into a variable data
+  mtrxData <- mtrx$get()
+  returnedData <- solve(mtrxData)
+  
+  ## set the inverse data of the matrix into returnedData variable
+  mtrx$setInversedData(returnedData)
+  
+  ## returned value with the inverse data
+  returnedData
 }
